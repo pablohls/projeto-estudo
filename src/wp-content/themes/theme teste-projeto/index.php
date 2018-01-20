8a94869932db9b87a8cc4fb5e4d4bf787d983118
Pablo Henrique <pablo.henrique_s@hotmail.com>
2018-01-20 14:47:14 -0200

    Arquivo Index alterado e Arquivo Single.php criado

--- src/wp-content/themes/theme teste-projeto/index.php
+++ src/wp-content/themes/theme teste-projeto/index.php
@@ -1,7 +1,11 @@
 <?php 
 
-while(){
-
+while(have_posts()){
+    the_post(); ?>
+    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <?php the_content(); ?>
+    <hr>
+    <?php
 }
 
 ?>
