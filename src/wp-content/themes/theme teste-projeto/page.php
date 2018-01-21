8b9423df8f39d5de98eee4d56520a08a7ec5a57a
Pablo Henrique <pablo.henrique_s@hotmail.com>
2018-01-21 08:49:36 -0200

    Alteração tema, estrutura do header e footer

--- src/wp-content/themes/theme teste-projeto/page.php
+++ src/wp-content/themes/theme teste-projeto/page.php
@@ -2,7 +2,8 @@
 get_header();
 while(have_posts()){
     the_post(); ?>
-    <h1><?php the_title(); ?></h1>
+    <h1>This is a page not a post</h1>
+    <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
     <?php
 }
